b7a00860b5107a58c52f3c8a5a5a103ff61f6d1f
Juan Jose Prieto Dzul <darkphoenix_46@hotmail.com>
2019-01-02 12:56:15 -0600

    Se corrigio erro que bloqueaba los menus al mostrar el dialogo

---
+++ Controller/PrintTicket.php
@@ -0,0 +1,96 @@
+<?php
+namespace FacturaScripts\Plugins\PrintTicket\Controller;
+
+use FacturaScripts\Core\App\AppSettings;
+use FacturaScripts\Core\Base\Controller;
+use FacturaScripts\Dinamic\Lib\TicketBuilder;
+use FacturaScripts\Plugins\PrintTicket\Model\Ticket;
+
+class PrintTicket extends Controller
+{
+    public $documentType;
+    public $documentCode;
+
+    public function getPageData()
+    {
+        $pageData = parent::getPageData();
+        $pageData['title'] = 'Configuracion Tickets';
+        $pageData['menu'] = 'admin';
+        $pageData['icon'] = 'fas fa-print';
+
+        return $pageData;
+    }
+
+    public function privateCore(&$response, $user, $permissions)
+    {
+        parent::privateCore($response, $user, $permissions);
+        $this->setTemplate('PrintTicketSettings');
+        $appSettings = new AppSettings();
+
+        $documentType = $this->request->query->get('documento');
+        if ($documentType != '') {
+            $this->setTemplate('PrintTicketScreen');
+            $this->documentType = $documentType;
+
+            $code = $this->request->query->get('code');
+            if ($code != '') {
+                
+                $className = 'FacturaScripts\\Dinamic\\Model\\' . $documentType;
+                $document = (new $className)->get($code);                
+                if ($document) {
+                    $this->buildTicket($document, $documentType);
+                }                
+            }
+            return;
+        }
+
+        $footerText = $this->request->request->get('footertext');
+        if ($footerText) {
+            $appSettings->set('ticket', 'footertext', $footerText);
+        }
+
+        $lineLength = $this->request->request->get('linelength');
+        if ($lineLength) {
+            $appSettings->set('ticket', 'linelength', $lineLength);
+        }
+        $appSettings->save();
+    }
+
+    private function buildTicket($document, $documentType)
+    {
+        $width = AppSettings::get('ticket', 'linelength');
+
+        switch ($documentType) {
+            case 'AlbaranCliente':
+                $builder = new TicketBuilder\TicketBuilderAlbaran($width); 
+                break;
+
+            case 'factura':
+                $builder = new TicketBuilder\TicketBuilderFactura($width);
+                break;
+            
+            case 'pedido':
+                $builder = new TicketBuilder\TicketBuilderPedido($width);
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+
+        if (isset($builder)) {
+            $footertext = AppSettings::get('ticket', 'footertext');
+
+            $builder->setCompany($this->empresa);
+            $builder->setDocument($document, $documentType);
+            //$builder->setCostumHeaderLines($this->headerLines); 
+            //$builder->setCostumFooterLines($this->footerLines);      
+            $builder->setFooterText($footertext);
+
+            $ticket = new Ticket();
+            $ticket->coddocument = $documentType;
+            $ticket->text = $builder->toString();
+            $ticket->save();            
+        }        
+    }
+}
--- Init.php
+++ Init.php
@@ -2,15 +2,11 @@
 namespace FacturaScripts\Plugins\PrintTicket;
 
 use FacturaScripts\Core\App\AppSettings;
+use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
 use FacturaScripts\Core\Base\EventManager;
 use FacturaScripts\Core\Base\InitClass;
-use FacturaScripts\Core\Base\MiniLog;
-use FacturaScripts\Core\Model\Empresa;
 use FacturaScripts\Core\Model\ApiKey;
-use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
 
-use FacturaScripts\Plugins\PrintTicket\Model\Ticket;
-use FacturaScripts\Dinamic\Lib\TicketBuilder;
 
 class Init extends InitClass
 {
@@ -18,11 +14,6 @@ class Init extends InitClass
     public function init()
     {
         /// código a ejecutar cada vez que carga FacturaScripts (si este plugin está activado).
-        EventManager::attach('Model:AlbaranCliente:save', function($albaran) {
-		    /// su código aquí
-		    /// $model contiene el artículo que se ha eliminado
-            $this->buildTicket($albaran, 'AlbaranCliente');		    
-		});
     }
 
     public function update()
@@ -35,47 +26,5 @@ class Init extends InitClass
             $apiKey->nick = 'admin';
             $apiKey->save();
         }
-    }
-
-    private function buildTicket($document, $type)
-    {
-        //$log = new MiniLog();
-        //$logMsg = sprintf("Imprimiendo %s <img src='http://localhost:10080?documento=%s alt='remote-printer'/>", $type, $type); 
-		//$log->notice($logMsg);
-
-        $width = AppSettings::get('ticket', 'linelength');
-        switch ($type) {
-            case 'AlbaranCliente':
-                $builder = new TicketBuilder\TicketBuilderAlbaran($width);
-                break;
-
-            case 'factura':
-                $builder = new TicketBuilderFactura($width);
-                break;
-            
-            case 'pedido':
-                $builder = new TicketBuilderPedido($width);
-                break;
-            
-            default:
-                # code...
-                break;
-        }
-
-        if (isset($builder)) {
-            $company = (new Empresa)->get(AppSettings::get('default', 'idempresa'));
-            $footertext = AppSettings::get('ticket', 'footertext');
-
-            $builder->setCompany($company);
-            $builder->setDocument($document, $type);
-            //$builder->setCostumHeaderLines($this->headerLines); 
-            //$builder->setCostumFooterLines($this->footerLines);      
-            $builder->setFooterText($footertext);
-
-            $ticket = new Ticket();
-			$ticket->coddocument = $type;
-			$ticket->text = $builder->toString();
-			$ticket->save();			
-        }        
     }
 }
